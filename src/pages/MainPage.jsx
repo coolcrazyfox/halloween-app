80edc02671d7058e286967942ec7ef89947148f7
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-10-22 21:14:42 +0300

    3

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import s from  '../styles/MainPage.module.css'
 
 const MainPage = () => {
     return (
-        <div cla>
-            Main
+        <div className={s.container}>
+            <div className={s.site_header}>
+                {/*<h1>Halooween</h1>*/}
+                <div className={s.spider }></div>
+                <div className={s.pumpkin}></div>
+                <div className={s.pumpkin_second}></div>
+                <div className={s.ghost}></div>
+                <div className={s.ghost1}></div>
+
+            </div>
+
         </div>
     );
 };
