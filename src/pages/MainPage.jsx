f02e3cfad55a0ca67d5133272c04db874645dba4
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-10-22 23:27:14 +0300

    4

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,6 +7,7 @@ const MainPage = () => {
             <div className={s.site_header}>
                 {/*<h1>Halooween</h1>*/}
                 <div className={s.spider }></div>
+                <div className={s.spider_second }></div>
                 <div className={s.pumpkin}></div>
                 <div className={s.ghost}></div>
                 <div className={s.ghost1}></div>
