07962848b92bcf3d2b084e25a6799e4195363e50
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-10-23 22:17:05 +0300

    changing all components

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import s from '../styles/UserPage.module.css'
+import  mainVideo from '../assets/happy-halloween.mp4'
+// import  mainVideo from '../assets/Halloween.mp4'
+// import  mainVideo from '../assets/Graveyard.mp4'
 const UserPage = () => {
     return (
-        <div>
+        <div className={s.main_container}>
+            <video autoPlay loop muted id='video' >
+                <source src={mainVideo} type='video/mp4'/>
+            </video>
+            <div className={s.content}>
+                <h1>Halloween</h1>
+                <p>And</p>
+                <h1>Party</h1>
+            </div>
 
         </div>
     );
