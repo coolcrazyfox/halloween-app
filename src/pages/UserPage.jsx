3450f7dff01b0a520ce03e7358abeefdf65db82d
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-10-23 22:22:00 +0300

    changing all components

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from '../styles/UserPage.module.css'
 import  mainVideo from '../assets/happy-halloween.mp4'
+import {Link} from "react-router-dom";
 // import  mainVideo from '../assets/Halloween.mp4'
 // import  mainVideo from '../assets/Graveyard.mp4'
 const UserPage = () => {
@@ -13,7 +14,11 @@ const UserPage = () => {
                 <h1>Halloween</h1>
                 <p>And</p>
                 <h1>Party</h1>
+                <Link to={'/'}>
+                    <button>Back </button>
+                </Link>
             </div>
+
 
         </div>
     );
